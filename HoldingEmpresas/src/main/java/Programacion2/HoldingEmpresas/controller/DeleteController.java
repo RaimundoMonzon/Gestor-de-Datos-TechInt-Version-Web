afc58fa3e79e887e0cdbbec00135e9301f1354fd
RaimundoMonzon <acari021@outlook.com>
2024-11-07 19:28:36 -0300

    Mensajes PopUp: - PopUp Service - PopUp agregados a la creacion y edicion de entidades. - PopUps agreagdos al registro de ingresos.

--- HoldingEmpresas/src/main/java/Programacion2/HoldingEmpresas/controller/DeleteController.java
+++ HoldingEmpresas/src/main/java/Programacion2/HoldingEmpresas/controller/DeleteController.java
@@ -1,24 +1,11 @@
 package Programacion2.HoldingEmpresas.controller;
 
-import java.sql.Date;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import Programacion2.HoldingEmpresas.entities.Administrador;
-import Programacion2.HoldingEmpresas.entities.Area;
-import Programacion2.HoldingEmpresas.entities.Asesor;
-import Programacion2.HoldingEmpresas.entities.Empresa;
-import Programacion2.HoldingEmpresas.entities.Pais;
-import Programacion2.HoldingEmpresas.entities.Rol;
-import Programacion2.HoldingEmpresas.entities.UserEntity;
-import Programacion2.HoldingEmpresas.entities.Vendedor;
 import Programacion2.HoldingEmpresas.services.AreaService;
 import Programacion2.HoldingEmpresas.services.EmpresaService;
 import Programacion2.HoldingEmpresas.services.PaisService;
@@ -35,14 +22,13 @@ public class DeleteController {
     private final UserService userService;
     private final PaisService paisService;
 
-
     @PostMapping("/user")
     public String editUser(@RequestParam Long id, @RequestParam String password, Model model) {
         if (userService.checkPassword(password)) {
             userService.delete(id);
             return "edit";
         }
-        
+
         model.addAttribute("error", "La contraseña no coincide");
         return "redirect:/edit/user";
     }
@@ -53,7 +39,7 @@ public class DeleteController {
             paisService.delete(id);
             return "edit";
         }
-        
+
         model.addAttribute("error", "La contraseña no coincide");
         return "edit/pais";
     }
@@ -64,7 +50,7 @@ public class DeleteController {
             areaService.delete(id);
             return "edit";
         }
-        
+
         model.addAttribute("error", "La contraseña no coincide");
         return "edit/area";
     }
@@ -75,7 +61,7 @@ public class DeleteController {
             empresaService.delete(id);
             return "edit";
         }
-        
+
         model.addAttribute("error", "La contraseña no coincide");
         return "edit/empresa";
     }
