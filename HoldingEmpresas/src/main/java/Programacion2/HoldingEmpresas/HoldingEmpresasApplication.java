74ac885db5ead1f1cd031be236d8d5e7411cae66
RaimundoMonzon <acari021@outlook.com>
2025-01-21 18:11:26 -0300

    Dockerized, Moving to Render to attempt Deployment

--- HoldingEmpresas/src/main/java/Programacion2/HoldingEmpresas/HoldingEmpresasApplication.java
+++ HoldingEmpresas/src/main/java/Programacion2/HoldingEmpresas/HoldingEmpresasApplication.java
@@ -2,15 +2,11 @@ package Programacion2.HoldingEmpresas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class HoldingEmpresasApplication {
 
 	public static void main(String[] args) {
-		@SuppressWarnings("unused")
-		Dotenv dotenv = Dotenv.configure().load();
 		SpringApplication.run(HoldingEmpresasApplication.class, args);
 	}
-
 }
